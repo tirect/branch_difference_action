45f92311f83a9a8d3cc425ef46de8ca5ac6b4ec1
Vitalii Vodkevych <tirect@gmail.com>
2024-07-05 15:01:43 +0300

    Added separation by teams

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ async function run() {
         const releaseBranch = core.getInput("release-branch", { required: true });
         const previousTag = core.getInput("previous-tag", { required: true });
         const slackWebhookUrl = core.getInput("slack-webhook-url", { required: true });
+        const teamMappingInput = core.getInput("team-mapping", { required: true });
+
+        const teamMapping = JSON.parse(teamMappingInput);
 
         if (typeof process.env.STALE_BRANCH_TOKEN === 'undefined') {
             throw new Error('GITHUB_TOKEN environment variable is not defined');
@@ -37,20 +40,68 @@ async function run() {
             });
         }
 
-        // Send a message for each author
-        for (const author in commitsByAuthor) {
-            let message = `Commits added to \`${releaseBranch}\` since \`${previousTag}\` by ${author}:\n`;
-            for (const commit of commitsByAuthor[author]) {
-                message += `\`${commit.sha}\` - ${commit.message}\n`;
-            }
+        const unassignedCommits: { [key: string]: any[] } = {};
 
-            const payload = JSON.stringify({ text: message });
+        // Send a message for each team lead
+        for (const lead in teamMapping) {
+            const teamInfo = teamMapping[lead];
+            let message = `Commits added to \`${releaseBranch}\` since \`${previousTag}\` by team members of ${teamInfo.team}:\n`;
+            let hasCommits = false;
+            for (const author of teamInfo.authors) {
+                if (commitsByAuthor[author]) {
+                    hasCommits = true;
+                    message += `Commits by ${author}:\n`;
+                    for (const commit of commitsByAuthor[author]) {
+                        message += `\`${commit.sha}\` - ${commit.message}\n`;
+                    }
+                }
+                
+                if (author != teamInfo.authors.last)
+                {
+                    message += `**********************\n`;
+                }
+            }
+            if (hasCommits) {
+                message += `\nTeam Lead: <@${lead}>`;
+
+                const payload = JSON.stringify({ text: message });
+
+                // Log the message to be sent to Slack
+                console.log("Message to be sent to Slack:", message);
+
+                // Send the message to Slack
+                await axios.default.post(slackWebhookUrl, payload);
+            }
+        }
+
+        // Handle unassigned commits
+        let unassignedMessage = `Commits added to \`${releaseBranch}\` since \`${previousTag}\` by unassigned authors:\n`;
+        let hasUnassignedCommits = false;
+        for (const author in commitsByAuthor) {
+            let isAssigned = false;
+            for (const lead in teamMapping) {
+                if (teamMapping[lead].authors.includes(author)) {
+                    isAssigned = true;
+                    break;
+                }
+            }
+            if (!isAssigned) {
+                hasUnassignedCommits = true;
+                unassignedMessage += `Commits by ${author}:\n`;
+                for (const commit of commitsByAuthor[author]) {
+                    unassignedMessage += `\`${commit.sha}\` - ${commit.message}\n`;
+                }
+            }
+        }
+
+        if (hasUnassignedCommits) {
+            const unassignedPayload = JSON.stringify({ text: unassignedMessage });
 
             // Log the message to be sent to Slack
-            console.log("Message to be sent to Slack:", message);
+            console.log("Unassigned message to be sent to Slack:", unassignedMessage);
 
             // Send the message to Slack
-            await axios.default.post(slackWebhookUrl, payload);
+            await axios.default.post(slackWebhookUrl, unassignedPayload);
         }
     } catch (error: any) {
         core.setFailed(error.message);
